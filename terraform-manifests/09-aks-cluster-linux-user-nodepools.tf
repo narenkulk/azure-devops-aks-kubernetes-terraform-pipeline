9ef0067357f83c29818560963c78da9e61f45b00
narenkulk <naren.kulk@gmail.com>
2021-09-02 13:59:23 -0400

    linux nodepool

--- terraform-manifests/09-aks-cluster-linux-user-nodepools.tf
+++ terraform-manifests/09-aks-cluster-linux-user-nodepools.tf
@@ -1,5 +1,5 @@
 # Create Linux Azure AKS Node Pool
-/*
+
 resource "azurerm_kubernetes_cluster_node_pool" "linux101" {
   availability_zones    = [1, 2, 3]
   enable_auto_scaling   = true
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "linux101" {
   os_disk_size_gb       = 30
   os_type               = "Linux" # Default is Linux, we can change to Windows
   vm_size               = "Standard_DS2_v2"
-  priority              = "Regular"  # Default is Regular, we can change to Spot with additional settings like eviction_policy, spot_max_price, node_labels and node_taints
+  priority              = "Regular" # Default is Regular, we can change to Spot with additional settings like eviction_policy, spot_max_price, node_labels and node_taints
   node_labels = {
     "nodepool-type" = "user"
     "environment"   = var.environment
@@ -26,4 +26,4 @@ resource "azurerm_kubernetes_cluster_node_pool" "linux101" {
     "app"           = "java-apps"
   }
 }
-*/
+
